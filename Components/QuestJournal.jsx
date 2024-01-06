26c1e2a0cefc95d201a639a58d19e599cedcbdf5
Adam Beck <mradamgbeck@gmail.com>
2024-01-06 18:32:00 -0500

    conditionally rendering quest journal

--- Components/QuestJournal.jsx
+++ Components/QuestJournal.jsx
@@ -7,15 +7,22 @@ import { useQuestsState } from '../state/QuestState.js';
 const QuestJournal = (navigation) => {
     let state = useQuestsState()
     const [quests, setQuests] = useState(state.get())
+    const renderQuests = (quests) => {
+        if (quests && quests.length > 0) {
+            return quests.map((quest, index) => {
+                return (
+                    <QuestCard key={index} quest={quest} />
+                )
+            })
+        } else {
+            return <Text style={styles.text}>Nothing to report! All Done.</Text>
+        }
+    }
     return (
         <View style={styles.appContainer}>
             <ScrollView style={styles.scrollView}>
                 {
-                    quests.map((quest, index) => {
-                        return (
-                            <QuestCard key={index} quest={quest} />
-                        )
-                    })
+                    renderQuests(quests)
                 }
             </ScrollView>
         </View>
